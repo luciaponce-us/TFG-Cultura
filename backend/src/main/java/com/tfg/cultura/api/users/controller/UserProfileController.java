623a5d97b062b72e6c896057a89eed5aee6ddfb9
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2026-05-04 14:26:55 +0000

    feat(backend): :sparkles: add operations to get, update, and delete profiles, and add file deletion to user deletion
    Refs #9

---
+++ backend/src/main/java/com/tfg/cultura/api/users/controller/UserProfileController.java
@@ -0,0 +1,76 @@
+package com.tfg.cultura.api.users.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tfg.cultura.api.users.model.dto.UserProfileUpdateRequest;
+import com.tfg.cultura.api.users.model.dto.UserResponse;
+import com.tfg.cultura.api.users.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users/profile")
+@Tag(name = "Users - Profile", description = "Gestiona tu perfil de usuario")
+@RequiredArgsConstructor
+public class UserProfileController {
+    private final UserService userService;
+    
+    @Operation(summary = "Obtener perfil", description = "Como usuario registrado, quiero poder ver los datos de mi usuario, para modificarlos si así lo deseo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario obtenido correctamente"),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Usuario no autenticado"),
+            @ApiResponse(responseCode = "404", description = "User Not Found - No se encontró el usuario")
+    })
+    @GetMapping
+    public ResponseEntity<UserResponse> getMyProfile() {
+        UserResponse response = userService.getProfile();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+
+    @Operation(summary = "RF-03: Modificar perfil", description = "Como usuario registrado, quiero poder modificar los datos de mi usuario, para mantener actualizada mi información")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario actualizado correctamente"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Solicitud inválida"),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Usuario no autenticado"),
+            @ApiResponse(responseCode = "404", description = "User Not Found - No se encontró el usuario")
+    })
+    @PutMapping
+    public ResponseEntity<UserResponse> updateMyProfile(@RequestBody @Valid UserProfileUpdateRequest request) {
+        UserResponse response = userService.updateProfile(request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+
+    @Operation(summary = "RF-07: Eliminación de mi usuario", description = "Como usuario registrado, quiero poder eliminar completamente mi cuenta, eliminando también mis datos del sistema, para sentirme más seguro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuario eliminado correctamente"),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Usuario no autenticado"),
+            @ApiResponse(responseCode = "404", description = "User Not Found - No se encontró el usuario")
+    })
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMyProfile() {
+        userService.deleteProfile();
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+
+}
--- backend/src/main/java/com/tfg/cultura/api/users/service/UserService.java
+++ backend/src/main/java/com/tfg/cultura/api/users/service/UserService.java
@@ -11,7 +11,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.tfg.cultura.api.suggestions.repository.SuggestionRepository;
 import com.tfg.cultura.api.users.exception.UserAlreadyExistsException;
 import com.tfg.cultura.api.users.exception.UserNotFoundException;
+import com.tfg.cultura.api.users.jwt.CustomUserDetails;
+import com.tfg.cultura.api.users.jwt.CustomUserDetailsService;
 import com.tfg.cultura.api.users.model.User;
+import com.tfg.cultura.api.users.model.dto.UserProfileUpdateRequest;
 import com.tfg.cultura.api.users.model.dto.UserResponse;
 import com.tfg.cultura.api.users.model.dto.UserUpdateRequest;
 import com.tfg.cultura.api.users.repository.UserRepository;
@@ -23,8 +26,9 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
+    private final CustomUserDetailsService userDetailsService;
     private final SuggestionRepository suggestionRepository;
+    private final UserFileService userFileService;
 
     private static final Logger logger = LoggerFactory.getLogger("usersLogger");
 
@@ -56,6 +60,65 @@ public class UserService {
     }
 
     public UserResponse updateUser(String username, UserUpdateRequest request) throws UserNotFoundException, UserAlreadyExistsException {
+        
+        User user = updateProfile(username, toProfileUpdateRequest(request));
+
+        if (isChanged(request.getDni(), user.getDni())) {
+            if (userRepository.existsByDni(request.getDni()))
+                throw new UserAlreadyExistsException("El DNI ya está en uso");
+
+            user.setDni(request.getDni());
+        }
+
+        if (request.getRole() != null) {
+            user.setRole(request.getRole());
+        }
+
+        user.setActive(request.isActive());
+
+        return saveUpdatedUser(user);
+    }
+
+    private boolean isChanged(String newValue, String currentValue) {
+        return newValue != null && !newValue.trim().equals(currentValue);
+    }
+
+    private UserProfileUpdateRequest toProfileUpdateRequest(UserUpdateRequest request) {
+        return UserProfileUpdateRequest.builder()
+                .username(request.getUsername())
+                .password(request.getPassword())
+                .name(request.getName())
+                .surname(request.getSurname())
+                .phone(request.getPhone())
+                .email(request.getEmail())
+                .build();
+    }
+
+    @Transactional
+    public void deleteUser(String username) throws UserNotFoundException {
+        User user = findUserByUsername(username);
+        userFileService.deleteUserFiles(user.getAvatar(), user.getPaymentReceipt());
+        suggestionRepository.deleteByAuthorId(user.getId());
+        userRepository.delete(user);
+        logger.info("Usuario con username {} eliminado correctamente", username);
+    }
+
+    // PROFILE
+
+    public UserResponse getProfile() throws UserNotFoundException {
+        CustomUserDetails currentUser = userDetailsService.getCurrentUserDetails();
+
+        return getUser(currentUser.getUsername());
+    }
+
+    public UserResponse updateProfile(UserProfileUpdateRequest request) throws UserNotFoundException, UserAlreadyExistsException {
+        CustomUserDetails currentUser = userDetailsService.getCurrentUserDetails();
+        User updatedUser = updateProfile(currentUser.getUsername(), request);
+
+        return saveUpdatedUser(updatedUser);
+    }
+
+    public User updateProfile(String username, UserProfileUpdateRequest request) throws UserNotFoundException, UserAlreadyExistsException {
         logger.info("Se va a actualizar el usuario con username {}", username);
         User user = findUserByUsername(username);
 
@@ -65,13 +128,6 @@ public class UserService {
 
             user.setUsername(request.getUsername());
             logger.info("Se ha cambiado el username de {} a {}", username, request.getUsername());
-        }
-
-        if (isChanged(request.getDni(), user.getDni())) {
-            if (userRepository.existsByDni(request.getDni()))
-                throw new UserAlreadyExistsException("El DNI ya está en uso");
-
-            user.setDni(request.getDni());
         }
 
         if (isChanged(request.getName(), user.getName())) {
@@ -94,27 +150,23 @@ public class UserService {
             user.setPassword(passwordEncoder.encode(request.getPassword()));
         }
 
-        if (request.getRole() != null) {
-            user.setRole(request.getRole());
-        }
+        return user;
+    }
 
-        user.setActive(request.isActive());
-
+    public UserResponse saveUpdatedUser(User user){
         User savedUser = userRepository.save(user);
-        logger.info("Usuario con username {} actualizado correctamente", username);
+        logger.info("Usuario con username {} actualizado correctamente", user.getUsername());
         return new UserResponse(savedUser);
     }
 
-    private boolean isChanged(String newValue, String currentValue) {
-        return newValue != null && !newValue.trim().equals(currentValue);
-    }
-
     @Transactional
-    public void deleteUser(String username) throws UserNotFoundException {
+    public void deleteProfile() throws UserNotFoundException {
+        CustomUserDetails currentUser = userDetailsService.getCurrentUserDetails();
+        String username = currentUser.getUsername();
         User user = findUserByUsername(username);
+        userFileService.deleteUserFiles(user.getAvatar(), user.getPaymentReceipt());
         suggestionRepository.deleteByAuthorId(user.getId());
         userRepository.delete(user);
         logger.info("Usuario con username {} eliminado correctamente", username);
     }
-
 }
