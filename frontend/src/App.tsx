743bc73582ba8f7358942e66b9fea204c142220f
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2026-05-08 18:08:20 +0000

    feat(frontend): :sparkles: add auth context to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./modules/core/pages/HomePage";
 import Layout from "./modules/core/Layout";
+import { AuthProvider } from "./modules/core/context/AuthProvider";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
---
+++ frontend/src/modules/core/context/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import { useMemo, useState } from "react";
+import type { ReactNode } from "react";
+import { AuthContext } from "./AuthContext";
+
+interface AuthProviderProps {
+  readonly children: ReactNode;
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token")
+  );
+
+  const login = (jwt: string) => {
+    localStorage.setItem("token", jwt);
+    setToken(jwt);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
+
+  const value = useMemo(
+    () => ({
+      token,
+      login,
+      logout,
+    }),
+    [token]
+  );
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
