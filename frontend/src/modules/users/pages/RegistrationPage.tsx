4d90d67e465cdd47788093c42c013e73a2b903ef
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2026-05-10 21:07:18 +0000

    feat(frontend): :sparkles: add step 2 for user registration form
    Closes #26

--- frontend/src/modules/users/pages/RegistrationPage.tsx
+++ frontend/src/modules/users/pages/RegistrationPage.tsx
@@ -1,5 +1,6 @@
-import { VStack, Heading, Flex } from "@chakra-ui/react";
+import { VStack, Heading, Flex, Text, HStack } from "@chakra-ui/react";
 import { useState, type ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { registerUser } from "../service/user.service";
 import type { UserRegisterRequest } from "../types";
 import { isApiError } from "@/modules/core/utils/utils";
@@ -10,11 +11,16 @@ import {
   CustomInput,
   UploadBox,
 } from "@/modules/core/components";
-import { IconArrowNarrowRight } from "@tabler/icons-react";
+import { IconArrowNarrowLeft, IconArrowNarrowRight } from "@tabler/icons-react";
 import * as validation from "../validations/user.validations";
 
+type RegistrationForm = UserRegisterRequest & {
+  confirmPassword: string;
+};
+
 export default function RegistrationPage() {
-  const [form, setForm] = useState<UserRegisterRequest>({
+  const navigate = useNavigate();
+  const [form, setForm] = useState<RegistrationForm>({
     username: "",
     password: "",
     name: "",
@@ -22,8 +28,8 @@ export default function RegistrationPage() {
     dni: "",
     phone: "",
     email: "",
+    confirmPassword: "",
   });
-
   const [avatar, setAvatar] = useState<File | null>(null);
   const [paymentReceipt, setPaymentReceipt] = useState<File | null>(null);
 
@@ -39,6 +45,7 @@ export default function RegistrationPage() {
   });
 
   const [step, setStep] = useState(1);
+  const [loadingRegister, setLoadingRegister] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setForm({
@@ -48,8 +55,24 @@ export default function RegistrationPage() {
   };
 
   const handleSubmit = async () => {
+    setErrors({
+      username: "",
+      password: "",
+      name: "",
+      surname: "",
+      dni: "",
+      phone: "",
+      email: "",
+      general: paymentReceipt ? "" : "La carta de pago es obligatoria",
+    });
+
+    const isValid = validateStep2(form);
+    if (!isValid || !paymentReceipt) return;
     try {
-      setErrors({
+      setLoadingRegister(true);
+      await registerUser(form, paymentReceipt, avatar || undefined);
+      setStep(3);
+      setForm({
         username: "",
         password: "",
         name: "",
@@ -57,29 +80,39 @@ export default function RegistrationPage() {
         dni: "",
         phone: "",
         email: "",
-        general: "",
+        confirmPassword: "",
       });
-      if (!paymentReceipt) {
-        setErrors((prev) => ({
-          ...prev,
-          general: "La carta de pago es obligatoria",
-        }));
-        return;
-      }
-      await registerUser(form, paymentReceipt);
-      alert("Usuario registrado y logueado");
     } catch (err) {
+      console.error("Error al registrar usuario:", err);
       if (isApiError(err))
-        setErrors({ ...errors, general: "No se pudo registrar" + err.message });
+        setErrors({ ...errors, general: "Error: " + err.message });
+    } finally {
+      setLoadingRegister(false);
     }
   };
 
-  const validateStep1 = (form: UserRegisterRequest): boolean => {
+  const validateStep1 = (form: RegistrationForm): boolean => {
     const newErrors = {
       name: validation.validateName(form.name),
       surname: validation.validateSurname(form.surname),
       dni: validation.validateDni(form.dni),
       general: paymentReceipt ? "" : "La carta de pago es obligatoria",
+    };
+
+    setErrors((prev) => ({ ...prev, ...newErrors }));
+    return !Object.values(newErrors).some((error) => error !== "");
+  };
+
+  const validateStep2 = (form: RegistrationForm): boolean => {
+    const newErrors = {
+      username: validation.validateUsername(form.username),
+      password: validation.validatePassword(
+        form.password,
+        true,
+        form.confirmPassword,
+      ),
+      email: validation.validateEmail(form.email),
+      phone: validation.validatePhone(form.phone),
     };
 
     setErrors((prev) => ({ ...prev, ...newErrors }));
@@ -99,17 +132,22 @@ export default function RegistrationPage() {
     >
       <VStack gap={4}>
         <Heading as="h1">Registro</Heading>
-
-        <TextSecondary>
-          Solo serán públicos tu nombre de usuario y foto de perfil
-        </TextSecondary>
-        {errors.general && (
-          <CustomAlert
-            state="error"
-            message={errors.general}
-            closeable={false}
-          />
+        {(step === 1 || step === 2) && (
+          <>
+            <TextSecondary>
+              Solo serán públicos tu nombre de usuario y foto de perfil
+            </TextSecondary>
+            {errors.general && (
+              <CustomAlert
+                state="error"
+                message={errors.general}
+                closeable={false}
+                maxWidth={"520px"}
+              />
+            )}
+          </>
         )}
+
         {step === 1 && (
           <>
             <CustomInput
@@ -119,6 +157,7 @@ export default function RegistrationPage() {
               required={true}
               error={errors.name}
               onChange={handleChange}
+              defaultValue={form.name}
             />
             <CustomInput
               label="Apellidos"
@@ -127,6 +166,7 @@ export default function RegistrationPage() {
               required={true}
               error={errors.surname}
               onChange={handleChange}
+              defaultValue={form.surname}
             />
             <CustomInput
               label="DNI"
@@ -135,6 +175,7 @@ export default function RegistrationPage() {
               required={true}
               error={errors.dni}
               onChange={handleChange}
+              defaultValue={form.dni}
             />
 
             <UploadBox
@@ -147,6 +188,9 @@ export default function RegistrationPage() {
               fileType="application/pdf"
               onFileChange={setPaymentReceipt}
             />
+            {paymentReceipt?.name && (
+              <Text fontSize="sm">Archivo subido: {paymentReceipt.name}</Text>
+            )}
 
             <CustomButton
               onClick={() => {
@@ -160,7 +204,107 @@ export default function RegistrationPage() {
             </CustomButton>
           </>
         )}
-        {step == 2 && <></>}
+        {step == 2 && (
+          <>
+            <CustomInput
+              label="Nombre de usuario"
+              name="username"
+              placeholder="Introduce tu nombre de usuario"
+              required={true}
+              error={errors.username}
+              onChange={handleChange}
+              defaultValue={form.username}
+            />
+            <CustomInput
+              label="Contraseña"
+              name="password"
+              required={true}
+              error={errors.password}
+              onChange={handleChange}
+              password={true}
+              defaultValue={form.password}
+            />
+            <CustomInput
+              label="Confirma tu contraseña"
+              name="confirmPassword"
+              required={true}
+              onChange={handleChange}
+              password={true}
+              defaultValue={form.confirmPassword}
+            />
+            <CustomInput
+              label="Correo electrónico"
+              name="email"
+              required={true}
+              error={errors.email}
+              onChange={handleChange}
+              defaultValue={form.email}
+            />
+            <CustomInput
+              label="Teléfono"
+              name="phone"
+              required={true}
+              error={errors.phone}
+              onChange={handleChange}
+              defaultValue={form.phone}
+            />
+
+            <UploadBox
+              text={
+                <>
+                  Arrastra tu <b>foto de perfil</b>
+                </>
+              }
+              secondaryText="JPG o PNG, tamaño no superior a 2MB"
+              fileType="image/*"
+              onFileChange={setAvatar}
+              disabled={loadingRegister}
+            />
+            {avatar?.name && (
+              <Text fontSize="sm">Archivo subido: {avatar.name}</Text>
+            )}
+            <HStack>
+              <CustomButton
+                onClick={() => {
+                  setStep(1);
+                }}
+                disabled={loadingRegister}
+              >
+                <IconArrowNarrowLeft stroke={2} /> Volver atrás
+              </CustomButton>
+              <CustomButton onClick={handleSubmit} loading={loadingRegister}>
+                Registrarse
+              </CustomButton>
+            </HStack>
+
+            <TextSecondary textAlign="center">
+              No podrás iniciar sesión hasta que un colaborador apruebe tu
+              registro. <br />
+              Muchas gracias por tu paciencia.
+            </TextSecondary>
+          </>
+        )}
+        {step == 3 && (
+          <>
+            <CustomAlert
+              state="success"
+              message="¡Registro solicitado correctamente!"
+              closeable={false}
+            />
+            <Text textAlign="center">
+              <b>RECUERDA:</b> <br />
+              No podrás iniciar sesión hasta que un colaborador apruebe tu
+              registro. <br />
+              Muchas gracias por tu paciencia.
+            </Text>
+            <CustomButton onClick={() => navigate("/iniciar-sesion")}>
+              Intentar iniciar sesión ahora
+            </CustomButton>
+            <CustomButton onClick={() => navigate("/")}>
+              Volver al inicio
+            </CustomButton>
+          </>
+        )}
       </VStack>
     </Flex>
   );
