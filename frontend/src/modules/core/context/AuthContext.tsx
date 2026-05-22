8eb003863162e8351960cc4cc0720137a090907e
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-05-22 15:32:44 +0000

    feat(front/core): :sparkles: add user info to auth context

--- frontend/src/modules/core/context/AuthContext.tsx
+++ frontend/src/modules/core/context/AuthContext.tsx
@@ -1,11 +1,11 @@
+import type { User } from "@/modules/users/types";
 import { createContext } from "react";
 
 export interface AuthContextType {
   token: string | null;
+  user: User | null;
   login: (jwt: string) => void;
   logout: () => void;
 }
 
-export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined,
-);
+export const AuthContext = createContext<AuthContextType | null>(null);
