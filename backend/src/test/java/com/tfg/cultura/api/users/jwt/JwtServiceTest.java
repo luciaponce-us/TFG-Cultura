3fec63a1a5694ae46347738d1fdecaa8c387006d
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-06-04 15:29:36 +0000

    test(backend): :white_check_mark: add JWT service test
    Added shouldReturnFalseWhenUserDetailsNotCustom

--- backend/src/test/java/com/tfg/cultura/api/users/jwt/JwtServiceTest.java
+++ backend/src/test/java/com/tfg/cultura/api/users/jwt/JwtServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import com.tfg.cultura.api.users.factory.UserFactory;
 import com.tfg.cultura.api.users.model.User;
 import com.tfg.cultura.api.users.model.enumerators.Role;
+import org.springframework.security.core.userdetails.UserDetails;
 
 class JwtServiceTest {
     private JwtService jwtService;
@@ -88,6 +89,14 @@ class JwtServiceTest {
         String token = jwtService.generateToken(username, role, id);
 
         assertFalse(jwtService.isTokenExpired(token));
+    }
+
+    @Test
+    void shouldReturnFalseWhenUserDetailsNotCustom() {
+        String token = jwtService.generateToken(username, role, id);
+        UserDetails loggedUser = null;
+
+        assertFalse(jwtService.isTokenValid(token, loggedUser));
     }
 
 }
