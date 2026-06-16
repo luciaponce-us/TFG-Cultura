a356581daaee4439cedc17f0fed9bd70a4c720a2
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-06-16 13:13:16 +0000

    refactor(backend): :memo: add interfaces to improve readability at user controllers

--- backend/src/main/java/com/tfg/cultura/api/users/controller/UserAuthController.java
+++ backend/src/main/java/com/tfg/cultura/api/users/controller/UserAuthController.java
@@ -1,6 +1,7 @@
 package com.tfg.cultura.api.users.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,11 +13,8 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import com.tfg.cultura.api.users.model.dto.UserLoginRequest;
@@ -26,22 +24,14 @@ import com.tfg.cultura.api.users.service.UserAuthService;
 
 @RestController
 @RequestMapping("/api/users/auth")
+@RequiredArgsConstructor
 @Tag(name = "Users - Auth", description = "Autenticación de usuarios")
-public class UserAuthController {
+public class UserAuthController implements UserAuthControllerInterface {
 
     private final UserAuthService userService;
 
-    public UserAuthController(UserAuthService userService) {
-        this.userService = userService;
-    }
-
-    @Operation(summary = "RF-01: Registrar usuarios", description = "Como usuario, quiero poder solicitar mi registro en el sistema, para poder iniciar sesión.")
+    @Override
     @PostMapping(value = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Registro correcto"),
-            @ApiResponse(responseCode = "409", description = "RN-01: DNI único por usuario / RN-02: Apodo único por usuario"),
-            @ApiResponse(responseCode = "400", description = "Los datos introducidos no son válidos")
-    })
     public ResponseEntity<UserResponse> register(
             @Valid @Parameter(description = "Datos del usuario en JSON", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)) @RequestPart("user") UserRegisterRequest request,
             @RequestPart(value = "avatar", required = false) MultipartFile avatar,
@@ -55,13 +45,7 @@ public class UserAuthController {
 
     }
 
-    @Operation(summary = "RF-02: Iniciar sesión", description = "Como usuario registrado, quiero poder iniciar sesión usando las credenciales con las que me registré previamente, para poder acceder a las funciones del sistema que requieran de autenticación")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Login correcto"),
-            @ApiResponse(responseCode = "401", description = "RN-04: Inicio de sesión - Unauthorized - Credenciales inválidas"),
-            @ApiResponse(responseCode = "403", description = "RN-04: Inicio de sesión - User Disabled"),
-            @ApiResponse(responseCode = "404", description = "RN-04: Inicio de sesión - User Not Found"),
-    })
+    @Override
     @PostMapping("/login")
     public ResponseEntity<String> login(@Valid @RequestBody UserLoginRequest request) {
         String token = userService.login(request);
---
+++ backend/src/main/java/com/tfg/cultura/api/users/controller/UserProfileControllerInterface.java
@@ -0,0 +1,45 @@
+package com.tfg.cultura.api.users.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.tfg.cultura.api.users.model.dto.UserProfileUpdateRequest;
+import com.tfg.cultura.api.users.model.dto.UserResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+
+public interface UserProfileControllerInterface {
+    
+    @Operation(summary = "Obtener perfil", description = "Como usuario registrado, quiero poder ver los datos de mi usuario, para modificarlos si así lo deseo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario obtenido correctamente"),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Usuario no autenticado"),
+            @ApiResponse(responseCode = "404", description = "User Not Found - No se encontró el usuario")
+    })
+    public ResponseEntity<UserResponse> getMyProfile();
+
+    @Operation(summary = "RF-03: Modificar perfil", description = "Como usuario registrado, quiero poder modificar los datos de mi usuario, para mantener actualizada mi información")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario actualizado correctamente"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Solicitud inválida"),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Usuario no autenticado"),
+            @ApiResponse(responseCode = "404", description = "User Not Found - No se encontró el usuario")
+    })
+    public ResponseEntity<UserResponse> updateMyProfile(@RequestBody @Valid UserProfileUpdateRequest request);
+
+    @Operation(summary = "Modificar mi avatar", description = "Como usuario registrado, quiero poder modificar la imagen de mi perfil, para personalizar mi cuenta")
+    public ResponseEntity<UserResponse> updateMyAvatar(@RequestPart(value = "avatar") MultipartFile avatar) ;
+
+    @Operation(summary = "RF-07: Eliminación de mi usuario", description = "Como usuario registrado, quiero poder eliminar completamente mi cuenta, eliminando también mis datos del sistema, para sentirme más seguro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuario eliminado correctamente"),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Usuario no autenticado"),
+            @ApiResponse(responseCode = "404", description = "User Not Found - No se encontró el usuario")
+    })
+    public ResponseEntity<Void> deleteMyProfile() ;
+}
