b9586debf14e012ae7db9736b607d601acb28a63
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-07-23 10:54:16 +0000

    fix(sections): :bug: use @PathVariable for section id endpoint
    Refs #32

--- backend/src/main/java/com/tfg/cultura/api/sections/controller/SectionController.java
+++ backend/src/main/java/com/tfg/cultura/api/sections/controller/SectionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,7 +45,7 @@ public class SectionController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SectionResponse> getSectionById(@RequestParam String id) {
+    public ResponseEntity<SectionResponse> getSectionById(@PathVariable String id) {
         SectionResponse response = sectionService.getSectionById(id);
         return ResponseEntity
                 .status(HttpStatus.OK)
