07722d960189a39482ced4a379702f349e6f1440
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-07-29 14:52:07 +0000

    feat(catalog): :sparkles: add Book model
    Refs #34

---
+++ backend/src/main/java/com/tfg/cultura/api/catalog/model/Book.java
@@ -0,0 +1,42 @@
+package com.tfg.cultura.api.catalog.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import com.tfg.cultura.api.catalog.model.enumerators.BookType;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Document(collection = "books")
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book extends Item {
+    @NotBlank(message = "El autor es obligatorio")
+    private String author;
+
+    @Pattern(regexp = "^(97(8|9))?\\d{9}(\\d|X)$", message = "El ISBN debe ser un número válido de 10 o 13 dígitos")
+    @NotBlank(message = "El ISBN es obligatorio")
+    private String isbn;
+
+    private String saga;
+    private Integer number;
+
+    @NotBlank(message = "El tipo es obligatorio")
+    private BookType type;
+
+    @DocumentReference
+    private Book prequel;
+    @DocumentReference
+    private Book sequel;
+
+}
---
+++ backend/src/main/java/com/tfg/cultura/api/catalog/model/enumerators/BookType.java
@@ -0,0 +1,10 @@
+package com.tfg.cultura.api.catalog.model.enumerators;
+
+public enum BookType {
+    NOVEL,
+    COMIC,
+    MANGA,
+    ENCYCLOPEDIA,
+    ROL
+    
+}
