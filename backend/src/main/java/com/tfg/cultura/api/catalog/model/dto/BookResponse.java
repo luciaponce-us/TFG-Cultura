ce48b6f6ff21f10cfca95c050f7e409568677422
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-08-05 18:13:42 +0000

    refactor(catalog): :recycle: add ItemResponse

--- backend/src/main/java/com/tfg/cultura/api/catalog/model/dto/BookResponse.java
+++ backend/src/main/java/com/tfg/cultura/api/catalog/model/dto/BookResponse.java
@@ -1,65 +1,34 @@
 package com.tfg.cultura.api.catalog.model.dto;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Set;
-
 import com.tfg.cultura.api.catalog.model.Book;
-import com.tfg.cultura.api.catalog.model.Category;
 import com.tfg.cultura.api.catalog.model.enumerators.BookType;
-import com.tfg.cultura.api.catalog.model.enumerators.ItemCondition;
-import com.tfg.cultura.api.sections.model.dto.SectionReference;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
-public class BookResponse {
-    // Item fields
-    private String id;
-    private String name;
-    private String description;
-    private String imageUrl;
-    private ItemCondition condition;
-    private String comments;
-    private Boolean loanAvailable;
-    private Boolean publicated;
-    private LocalDate purchasedAt;
-    private BigDecimal price;
-    private Integer copies;
-    private Integer availableCopies;
-    private Integer loanDays;
-    private SectionReference section;
-    private Set<Category> categories;
-    private LocalDateTime createdAt;
-    // Book-specific fields
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class BookResponse extends ItemResponse {
+
     private String author;
     private String isbn;
     private BookType type;
     private String saga;
 
     public BookResponse(Book book) {
-        this.id = book.getId();
-        this.name = book.getName();
-        this.description = book.getDescription();
-        this.imageUrl = book.getImageUrl();
-        this.condition = book.getCondition();
-        this.comments = book.getComments();
-        this.loanAvailable = book.getLoanAvailable();
-        this.publicated = book.getPublicated();
-        this.purchasedAt = book.getPurchasedAt();
-        this.price = book.getPrice();
-        this.copies = book.getCopies();
-        this.availableCopies = book.getAvailableCopies();
-        this.loanDays = book.getLoanDays();
-        this.section = book.getSection() != null ? new SectionReference(book.getSection()) : null;
-        this.categories = book.getCategories();
-        this.createdAt = book.getCreatedAt();
+        super(book);
 
-        // Book-specific fields
         this.author = book.getAuthor();
         this.isbn = book.getIsbn();
         this.type = book.getType();
-        this.saga = book.getSaga() != null ? book.getSaga().getName() : null;
+        this.saga = book.getSaga() != null
+                ? book.getSaga().getName()
+                : null;
     }
 }
