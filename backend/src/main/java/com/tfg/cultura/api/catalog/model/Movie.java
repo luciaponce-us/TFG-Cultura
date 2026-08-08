15709082249781e7b144164446c0da9cf6b11cfe
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-08-08 16:29:21 +0000

    feat(catalog): :sparkles: add movie model validations
    Refs #34

--- backend/src/main/java/com/tfg/cultura/api/catalog/model/Movie.java
+++ backend/src/main/java/com/tfg/cultura/api/catalog/model/Movie.java
@@ -1,11 +1,13 @@
 package com.tfg.cultura.api.catalog.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import com.tfg.cultura.api.catalog.model.enumerators.Format;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,8 +20,16 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie extends Item {
+    
+    @NotNull(message = "El formato es obligatorio")
     private Format format;
-    private Integer numberOfDiscs;
+
+    @NotNull(message = "El número de discos es obligatorio")
+    @Min(value = 1, message = "El número de discos debe ser mayor o igual a 1")
+    @Builder.Default
+    private Integer numberOfDiscs = 1;
+
+    @NotNull(message = "La información de la película es obligatoria")
     private MovieInfo movieInfo;
     
 }
--- backend/src/main/java/com/tfg/cultura/api/catalog/model/MovieInfo.java
+++ backend/src/main/java/com/tfg/cultura/api/catalog/model/MovieInfo.java
@@ -2,14 +2,26 @@ package com.tfg.cultura.api.catalog.model;
 
 import java.time.LocalDate;
 
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class MovieInfo {
+
+    @Past(message = "La fecha de lanzamiento debe ser una fecha pasada")
+    @NotNull(message = "La fecha de estreno es obligatoria")
     private LocalDate releaseDate;
+
+    @Size(max = 500, message = "La URL del tráiler no puede superar los 500 caracteres")
     private String trailerUrl;
+
+    @DocumentReference
     private Saga saga;
     
 }
