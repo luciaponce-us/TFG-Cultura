5eaa4945fcf5cc484bb01ed96722c309f4c897c0
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-08-09 16:15:52 +0000

    feat(catalog): :sparkles: add BoardGame model and repository
    Refs #34

---
+++ backend/src/main/java/com/tfg/cultura/api/catalog/model/enumerators/BoardGameType.java
@@ -0,0 +1,14 @@
+package com.tfg.cultura.api.catalog.model.enumerators;
+
+public enum BoardGameType {
+    STRATEGY,
+    FAMILY,
+    PARTY,
+    COOPERATIVE,
+    CARD_GAME,
+    DICE_GAME,
+    MINIATURES,
+    ABSTRACT,
+    WARGAME
+    
+}
---
+++ backend/src/main/java/com/tfg/cultura/api/catalog/model/enumerators/Complexity.java
@@ -0,0 +1,7 @@
+package com.tfg.cultura.api.catalog.model.enumerators;
+
+public enum Complexity {
+    EASY,
+    MEDIUM,
+    HARD
+}
