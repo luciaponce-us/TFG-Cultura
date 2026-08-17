c1fa0c0d3348518ef3a342afccb84c7958cf01f4
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-08-17 16:53:47 +0000

    feat(catalog): :sparkles: add board game controller
    Refs #34

---
+++ backend/src/main/java/com/tfg/cultura/api/catalog/controller/BoardGameController.java
@@ -0,0 +1,21 @@
+package com.tfg.cultura.api.catalog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tfg.cultura.api.catalog.model.dto.BoardGameRequest;
+import com.tfg.cultura.api.catalog.model.dto.BoardGameResponse;
+import com.tfg.cultura.api.catalog.service.BoardGameService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/catalog/board-games")
+@Tag(name="Catalog - Board Games", description = "Gestión de juegos de mesa")
+public class BoardGameController extends AbstractItemController<BoardGameRequest,BoardGameResponse,BoardGameService>{
+    
+    public BoardGameController(BoardGameService service){
+        super(service);
+    }
+    
+}
