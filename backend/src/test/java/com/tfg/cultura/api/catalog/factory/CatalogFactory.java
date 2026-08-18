cf58ca96d22a974222f69933c2ea444c4b94a31d
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-08-18 12:17:53 +0000

    test(catalog): :white_check_mark: add series service tests
    Refs #34

--- backend/src/test/java/com/tfg/cultura/api/catalog/factory/CatalogFactory.java
+++ backend/src/test/java/com/tfg/cultura/api/catalog/factory/CatalogFactory.java
@@ -1,6 +1,11 @@
 package com.tfg.cultura.api.catalog.factory;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -10,10 +15,16 @@ import com.tfg.cultura.api.catalog.model.Category;
 import com.tfg.cultura.api.catalog.model.Movie;
 import com.tfg.cultura.api.catalog.model.MovieInfo;
 import com.tfg.cultura.api.catalog.model.Saga;
+import com.tfg.cultura.api.catalog.model.Season;
+import com.tfg.cultura.api.catalog.model.Series;
+import com.tfg.cultura.api.catalog.model.SeriesInfo;
 import com.tfg.cultura.api.catalog.model.dto.BookRequest;
 import com.tfg.cultura.api.catalog.model.dto.MovieRequest;
+import com.tfg.cultura.api.catalog.model.dto.SeriesRequest;
 import com.tfg.cultura.api.catalog.model.enumerators.BookType;
 import com.tfg.cultura.api.catalog.model.enumerators.Format;
+import com.tfg.cultura.api.catalog.model.enumerators.SeriesStatus;
+import com.tfg.cultura.api.sections.factory.SectionFactory;
 
 public class CatalogFactory {
 
@@ -74,6 +85,60 @@ public class CatalogFactory {
                 .trailerUrl("https://example.com/trailer")
                 .sagaName(validSaga().getName())
                 .build();
+    }
+
+    public static Season validSeason() {
+        return Season.builder()
+                .seasonNumber(1)
+                .seasonPart(1)
+                .trailerUrl("https://example.com/season-trailer")
+                .build();
+    }
+
+    public static Series validSeries() {
+
+        SeriesInfo info = SeriesInfo.builder()
+                .releaseDate(LocalDate.of(2011, 04, 17))
+                .numberOfSeasons(8)
+                .status(SeriesStatus.FINISHED)
+                .build();
+
+        Season season = validSeason();
+
+        return Series.builder()
+                .name("Juego de Tronos")
+                .purchasedAt(LocalDate.of(2015, 01, 01)) // Optional
+                .price(BigDecimal.valueOf(15.99)) // Optional
+                .loanDays(14) // Optional
+                .section(SectionFactory.validSection()) // Optional
+                .categories(Set.of(validCategory())) // Optional
+                // Series fields
+                .format(Format.DVD)
+                .numberOfDiscs(2)
+                .seriesInfo(info)
+                .seasons(new ArrayList<Season>(List.of(season)))
+                .build();
+    }
+
+    public static SeriesRequest validSeriesRequest() {
+        Series series = validSeries();
+        Set<String> categoriesIds = series.getCategories().stream().map(c -> c.getId()).collect(Collectors.toSet());
+
+        return SeriesRequest.builder()
+                .name(series.getName())
+                .purchasedAt(series.getPurchasedAt()) // Optional
+                .price(series.getPrice()) // Optional
+                .sectionId(series.getSection().getId()) // Optional
+                .categoriesIds(categoriesIds) // Optional
+                // Series fields
+                .format(series.getFormat())
+                .numberOfDiscs(series.getNumberOfDiscs())
+                .releaseDate(series.getSeriesInfo().getReleaseDate())
+                .numberOfSeasons(series.getSeriesInfo().getNumberOfSeasons())
+                .status(series.getSeriesInfo().getStatus())
+                .seasons(series.getSeasons())
+                .build();
+
     }
 
     public static MockMultipartFile mockImagePart() {
