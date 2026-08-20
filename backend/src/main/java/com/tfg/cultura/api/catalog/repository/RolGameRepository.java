9fa0ea27d53dfa428b07ce570fd26c8a5865bbd0
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-08-20 12:29:22 +0000

    feat(catalog): :sparkles: add basic RolGame and RolSaga repositories
    Refs #34

---
+++ backend/src/main/java/com/tfg/cultura/api/catalog/repository/RolGameRepository.java
@@ -0,0 +1,7 @@
+package com.tfg.cultura.api.catalog.repository;
+
+import com.tfg.cultura.api.catalog.model.RolGame;
+
+public interface RolGameRepository extends ItemRepository<RolGame> {
+    
+}
---
+++ backend/src/main/java/com/tfg/cultura/api/catalog/repository/RolSagaRepository.java
@@ -0,0 +1,9 @@
+package com.tfg.cultura.api.catalog.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.tfg.cultura.api.catalog.model.RolSaga;
+
+public interface RolSagaRepository extends MongoRepository<RolSaga, String> {
+    
+}
