459d0ddd16723feae6efeb404e46818892ffd379
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2026-08-25 10:21:07 +0000

    fix(catalog): :fire: remove broken global item search
    Remove the global item search feature as its current implementation is too complex and does not work reliably.
    
    Refs #34

--- backend/src/main/java/com/tfg/cultura/api/catalog/controller/ItemController.java
+++ backend/src/main/java/com/tfg/cultura/api/catalog/controller/ItemController.java
@@ -1,19 +1,13 @@
 package com.tfg.cultura.api.catalog.controller;
 
 import java.util.List;
-import java.util.Set;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tfg.cultura.api.catalog.model.Item;
 import com.tfg.cultura.api.catalog.service.ItemService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -34,15 +28,5 @@ public class ItemController {
         .status(HttpStatus.OK)
         .body(itemTypes);
     }
-
-    @GetMapping
-    public ResponseEntity<Page<Item>> getAllItems(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(required = false) String nameContains, @RequestParam(required = false) Set<String> categoryIds, @RequestParam(required = false) String sectionId) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Item> items = itemService.getAllItems(pageable, nameContains, categoryIds, sectionId);
-        return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(items);
-    }
    
-    
 }
--- backend/src/main/java/com/tfg/cultura/api/catalog/service/ItemService.java
+++ backend/src/main/java/com/tfg/cultura/api/catalog/service/ItemService.java
@@ -1,28 +1,11 @@
 package com.tfg.cultura.api.catalog.service;
 
 import java.util.List;
-import java.util.Set;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.tfg.cultura.api.catalog.model.Category;
-import com.tfg.cultura.api.catalog.model.Item;
-import com.tfg.cultura.api.catalog.repository.ItemRepository;
-import com.tfg.cultura.api.sections.exception.SectionNotFoundException;
-import com.tfg.cultura.api.sections.model.Section;
-import com.tfg.cultura.api.sections.service.SectionService;
-
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class ItemService {
-
-    private final ItemRepository itemRepository;
-    private final SectionService sectionService;
-    private final CategoryService categoryService;
 
     public List<String> getItemTypes() {
         return List.of(
@@ -32,19 +15,6 @@ public class ItemService {
             "Serie",
             "Videojuego",
             "Juego de rol");
-    }
-
-    public Page<Item> getAllItems(Pageable pageable, String nameContains, Set<String> categoryIds, String sectionId) throws SectionNotFoundException {
-        
-        Section section = sectionId == null ? null : sectionService.findSectionById(sectionId);
-        Set<Category> categories = categoryIds == null ? Set.of() : categoryService.findCategoriesByIds(categoryIds);
-        String nameFilter = nameContains == null ? "*" : nameContains;
-
-        return itemRepository.findAllByNameContainingIgnoreCaseAndCategoriesContainingAndSection(
-            nameFilter,
-            categories,
-            section,
-            pageable);
     }
 
 }
